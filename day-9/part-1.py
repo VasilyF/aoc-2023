2b649eec45bc4579febe3f434d23da48472760c6
vasily <vasilyfedotov12@gmail.com>
2023-12-09 18:52:54 -0500

    day 9 - parts 1 and 2

---
+++ day-9/part-1.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+def all_zeros(it):
+    for elem in it:
+        if elem != 0:
+            return False
+    return True
+
+
+def extrapolate(nums):
+    if all_zeros(nums):
+        return 0
+
+    diffs = []
+    for i in range(len(nums) - 1):
+        diffs.append(nums[i+1] - nums[i])
+
+    return nums[-1] + extrapolate(diffs)
+
+
+with open('input.txt') as f:
+    lines = f.readlines()
+
+res = 0
+for line in lines:
+    nums = [int(x) for x in line.split()]
+    res += extrapolate(nums) 
+
+print(res)
